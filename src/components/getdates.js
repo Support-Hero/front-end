d716a75b146dcb6b91c36882662f8529f016a9a5
elle-n-lu <joew1960938924@gmail.com>
2023-09-02 15:28:57 +1000

    add rosrer fetch, create,update

--- src/components/getdates.js
+++ src/components/getdates.js
@@ -2,7 +2,7 @@ export const getDateList=()=>{
     const datelist=[]
     //get this week's date & day
     const today=new Date()
-    for(let i=7;i>=0;i--){
+    for(let i=12;i>=0;i--){
         const d = new Date()
         const today=new Date()
         d.setDate(d.getDate() + (i - d.getDay()));
@@ -14,11 +14,14 @@ export const getDateList=()=>{
     }
     datelist.reverse()
     datelist.unshift(today.getFullYear()+'-'+((today.getMonth()+1).toString().length>1 ?(today.getMonth()+1):'0'+(today.getMonth()+1)) +'-'+(today.getDate().toString().length>1?today.getDate():('0'+today.getDate())))
+    // console.log('dat1',datelist)
 
-    for(let i=8;i<=14;i++){
+    for(let i=1;i<=7;i++){
         const d = new Date()
-        d.setDate(d.getDate() + (i - d.getDay()));
+        d.setDate(d.getDate() + (i + d.getDay()));
         datelist.push(d.getFullYear()+'-'+((d.getMonth()+1).toString().length>1 ? (d.getMonth()+1): '0'+(d.getMonth()+1)) +'-'+(d.getDate().toString().length>1?d.getDate():('0'+d.getDate())))
     }
+    // console.log('dat2',datelist)
+
     return datelist
 }
--- src/modals/RosterCreate.jsx
+++ src/modals/RosterCreate.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
 import './rostercreate.css'
-const RosterCreate = ({ setWorkerName, workerName, date, setDate, setOpen }) => {
-    const createshiftModal = (setWorkerName, workerName, date, setDate, setOpen) => (
+import { api } from '../api'
+const RosterCreate = ({
+    token,
+    workerlist,
+    setOpen,
+    workerName, setWorkerName,
+    shiftStart, setShiftStart,
+    shiftEnd, setShiftEnd,
+    breakEnd, setBreakEnd,
+    breakStart, setBreakStart,
+    breakStatus, setBreakStatus,
+    date, setDate
+}) => {
+    const addnewShift = async (token,
+        setOpen,
+        workerName, date,
+        shiftStart, shiftEnd,
+        breakStart, breakEnd, breakStatus) => {
+        const res = await fetch(api + "/rosters/", {
+            method: "POST",
+            body: JSON.stringify(
+                {
+                    worker: workerName,
+                    date: date,
+                    shiftStart: shiftStart,
+                    shiftEnd: shiftEnd,
+                    breakStart: breakStart,
+                    breakEnd, breakEnd,
+                    break: breakStatus
+                }
+            ),
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = await res.json()
+        window.location.reload()
+        // reset all input
+        setOpen(false)
+
+    }
+    const createshiftModal = (
+        token,
+        workerlist, setOpen,
+        workerName, setWorkerName,
+        shiftStart, setShiftStart,
+        shiftEnd, setShiftEnd,
+        breakEnd, setBreakEnd,
+        breakStart, setBreakStart,
+        breakStatus, setBreakStatus,
+        date, setDate) => (
         <div className='bg-black w-100 bg-opacity-75 pt-5' style={{ height: "120vh", position: "absolute", top: "0", zIndex: 1 }}>
             <div className="bg-white mx-auto p-5" id='modal-box'>
                 <form
@@ -9,8 +59,11 @@ const RosterCreate = ({ setWorkerName, workerName, date, setDate, setOpen }) => 
                     onSubmit={(e) => {
                         e.preventDefault();
                         addnewShift(
+                            token,
                             setOpen,
                             workerName, date,
+                            shiftStart, shiftEnd,
+                            breakStart, breakEnd, breakStatus
                         );
                     }}
                 >
@@ -18,17 +71,14 @@ const RosterCreate = ({ setWorkerName, workerName, date, setDate, setOpen }) => 
                     <hr />
                     <div className="mb-3">
                         <label htmlFor="workername" className="form-label"> worker name:</label>
-                        <input
-                            required
-                            id='workername'
-                            aria-label='workername'
-                            value={workerName}
-                            onChange={(e) => {
-                                e.preventDefault();
-                                setWorkerName(e.target.value);
-                            }}
-                            className="form-control"
-                        />
+                        <select value={workerName} className="mx-1" onChange={(e) => setWorkerName(e.target.value)}>
+                            <option value="-">-Select-</option>
+                            {
+                                workerlist.map((name, index) => (
+                                    <option value={name._id} key={index}>{name.firstName}{" "}{name.lastName}</option>
+                                ))
+                            }
+                        </select>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="date" className="form-label">
@@ -49,68 +99,82 @@ const RosterCreate = ({ setWorkerName, workerName, date, setDate, setOpen }) => 
                     </div>
                     <div className="d-flex mb-3 ">
                         <div className="d-flex flex-column " style={{ marginRight: "20px" }}>
-                            <label htmlFor="Shift Start" className="form-label">
+                            <label htmlFor="shiftStart" className="form-label">
                                 Shift Start
                             </label>
                             <input required
-                            id='Shift Start'
+                                id='shiftStart'
                                 className="form-control"
                                 type="time"
-                                //   value={shiftStart}
+                                aria-label='shiftStart'
+                                value={shiftStart}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    // setShiftStart(e.target.value);
+                                    setShiftStart(e.target.value);
                                 }}
                             />
                         </div>
-                        <div className="d-flex flex-column">
-                            <label htmlFor="Shift End" className="form-label">
+                        <div className="d-flex flex-column " style={{ marginRight: "20px" }}>
+                            <label htmlFor="shiftEnd" className="form-label">
                                 Shift End
                             </label>
                             <input required
-                            id='Shift End'
+                                id='shiftEnd'
                                 className="form-control"
                                 type="time"
-                                //   value={shiftEnd}
+                                aria-label='shiftEnd'
+                                value={shiftEnd}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    // setShiftEnd(e.target.value);
+                                    setShiftEnd(e.target.value);
                                 }}
                             />
                         </div>
                     </div>
                     <div className="d-flex mb-3">
+                        <label htmlFor="breakStart" className="form-label">
+                            Break:
+                        </label>
+                        <select value={breakStatus} className="mx-1" onChange={(e) => setBreakStatus(e.target.value)}>
+                            <option value="-">-Select-</option>
+                            <option value="false">No</option>
+                            <option value='true'>Yes</option>
+                        </select>
+                    </div>
+                    {breakStatus && <div className="d-flex mb-3">
                         <div className="d-flex flex-column" style={{ marginRight: "20px" }}>
-                            <label htmlFor="Break Start" className="form-label">
+                            <label htmlFor="breakStart" className="form-label">
                                 Break Start
                             </label>
                             <input
-                            id='Break Start'
+                                id='breakStart'
+                                aria-label='breakStart'
                                 className="form-control"
                                 type="time"
-                                //   value={breakStart}
+                                value={breakStart}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    // setBreakStart(e.target.value);
+                                    setBreakStart(e.target.value);
                                 }}
                             />
                         </div>
                         <div className="d-flex flex-column">
-                            <label htmlFor="Break End" className="form-label">
+                            <label htmlFor="breakEnd" className="form-label">
                                 Break End
                             </label>
                             <input required
-                            id='Break End'
+                                id='breakEnd'
+                                aria-label='breakEnd'
                                 className="form-control"
                                 type="time"
-                                //   value={breakEnd}
+                                value={breakEnd}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    // setBreakEnd(e.target.value);
+                                    setBreakEnd(e.target.value);
                                 }}
                             />
                         </div>
-                    </div>
+                    </div>}
                     <div className="d-flex justify-content-md-between">
                         <button
                             className="btn btn-secondary"
@@ -118,6 +182,11 @@ const RosterCreate = ({ setWorkerName, workerName, date, setDate, setOpen }) => 
                                 e.preventDefault();
                                 setWorkerName()
                                 setDate()
+                                setShiftStart()
+                                setShiftEnd()
+                                setBreakStart()
+                                setBreakEnd()
+                                setBreakStatus(false)
                                 setOpen(false)
                             }}
                         >
@@ -133,7 +202,14 @@ const RosterCreate = ({ setWorkerName, workerName, date, setDate, setOpen }) => 
     )
     return (
         <>
-            {createshiftModal(setWorkerName, workerName, date, setDate, setOpen)}
+            {createshiftModal(token, workerlist, setOpen,
+                workerName, setWorkerName,
+                shiftStart, setShiftStart,
+                shiftEnd, setShiftEnd,
+                breakEnd, setBreakEnd,
+                breakStart, setBreakStart,
+                breakStatus, setBreakStatus,
+                date, setDate)}
         </>)
 }
 export default RosterCreate
