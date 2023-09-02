196f6aadc508814f9cd767973c61dd0a44daa019
CameronWD <14041@coderacademy.edu.au>
2023-09-02 15:15:02 +1000

    added approval/reject to manager notes, with only non-assessed notes being displayed, updated index title for browser, updated workerdashboard stlying to match rest of site and made entire buttons clickable

--- src/CaseNoteApproval/casenoteapproval.jsx
+++ src/CaseNoteApproval/casenoteapproval.jsx
@@ -1,55 +1,90 @@
-import React, { useState } from 'react';
-import { dummyNotes } from '../dummy';
+import React, { useState, useEffect } from 'react';
+import { dummyNotes } from '../dummy.js';
 import Navbar from '../components/Navbar/Navbar';
 import { Link } from 'react-router-dom';
 
 const CaseNoteApproval = () => {
-  const [notes, setNotes] = useState();
+  const [notes, setNotes] = useState([]);
   const [selectedNote, setSelectedNote] = useState(null);
   const [confirmationMessage, setConfirmationMessage] = useState('');
 
-  const searchNote = () => {};
-  
+  useEffect(() => {
+    setNotes(dummyNotes);
+  }, []);
+
   const approveNote = (id) => {
-    console.log(`Note with id ${id} is approved`);
-    setConfirmationMessage(`Note with id ${id} is approved`);
+    if (id === undefined || id === null) {
+      console.error("Undefined or null ID provided to approveNote");
+      return;
+    }
+    const updatedNotes = notes.map((note) => {
+      if (note && note.id === id) {
+        return { ...note, isMgrAuthorised: true };
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+    setConfirmationMessage({ message: `Progress note with the id ${id} is approved`, type: 'success' });
   };
-  
+
   const rejectNote = (id) => {
-    console.log(`Note with id ${id} is rejected`);
-    setConfirmationMessage(`Note with id ${id} is rejected`);
+    if (id === undefined || id === null) {
+      console.error("Undefined or null ID provided to rejectNote");
+      return;
+    }
+    const updatedNotes = notes.map((note) => {
+      if (note && note.id === id) {
+        return { ...note, isMgrAuthorised: false };
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+    setConfirmationMessage({ message: `Progress note with the id ${id} is rejected`, type: 'danger' });
   };
 
   const viewNoteDetails = (note) => {
     setSelectedNote(note);
   };
 
+
+  const unapprovedNotes = notes.filter(note => !note.isMgrAuthorised);
+
   return (
     <div>
       <Navbar />
       <div className="container mt-5 p-0">
-        <div id="second_nav_out" className="d-flex justify-content-between">
-          <label className="fs-3">Case Note Approval</label>
-          <form className="d-flex" id="manager-search-form" onSubmit={searchNote}>
-            <input className="form-control mr-1" type="search" placeholder="Search" aria-label="Search" />
-            <select className="mx-1">
-              <option value="-">-Select-</option>
-              <option value="author">Author</option>
-              <option value="client">Client</option>
-            </select>
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-        <hr />
-      </div>
-      {confirmationMessage && 
-        <div className={`alert mt-3 ${confirmationMessage.includes('approved') ? 'alert-success' : 'alert-danger'}`}>
-          {confirmationMessage}
-        </div>
-      }
-      {notes ? (
-        <>Notes fetched for approval</>
-      ) : (
+      {confirmationMessage.message && 
+          <div className={`alert alert-${confirmationMessage.type === 'success' ? 'success' : 'danger'}`}>
+            {confirmationMessage.message}
+          </div>
+        }
+        {selectedNote && (
+          <div className="modal d-block">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Note Details</h5>
+                  <button type="button" className="btn-close" onClick={() => setSelectedNote(null)}></button>
+                </div>
+                <div className="modal-body">
+                  <p><strong>ID:</strong> {selectedNote.id}</p>
+                  <p><strong>Date:</strong> {selectedNote.date}</p>
+                  <p><strong>Goals:</strong> {selectedNote.goals}</p>
+                  <p><strong>Presentation:</strong> {selectedNote.presentation}</p>
+                  <p><strong>Actions:</strong> {selectedNote.actions}</p>
+                  <p><strong>Outcome:</strong> {selectedNote.outcome}</p>
+                  <p><strong>Follow-Up:</strong> {selectedNote.followUp}</p>
+                  <p><strong>Follow-Up Note:</strong> {selectedNote.followUpNote}</p>
+                  <p><strong>Author:</strong> {selectedNote.author}</p>
+                  <p><strong>Client ID:</strong> {selectedNote.client_id}</p>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" onClick={() => setSelectedNote(null)}>Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <table className="table container">
           <thead>
             <tr>
@@ -58,20 +93,20 @@ const CaseNoteApproval = () => {
               <th scope="col">Date</th>
               <th scope="col">Client</th>
               <th scope="col">Goals</th>
-              <th scope="col">Follow-up</th>
+              <th scope="col">Status</th>
               <th scope="col">Action</th>
               <th scope="col">View Entry</th>
             </tr>
           </thead>
           <tbody>
-            {dummyNotes.map((note, index) => (
-              <tr key={index}>
+            {unapprovedNotes.map((note, index) => (
+              <tr key={note.id.toString()}>
                 <th scope="row">{note.id}</th>
                 <td>{note.author}</td>
                 <td>{note.date}</td>
                 <td>{note.client_id}</td>
                 <td>{note.goals.slice(0, 30)}...</td>
-                <td><Link to="#">{note.followUp}</Link></td>
+                <td>{note.isMgrAuthorised ? "Approved" : "Rejected"}</td>
                 <td>
                   <button className="btn btn-success" onClick={() => approveNote(note.id)}>Approve</button>
                   <button className="btn btn-danger ml-2" onClick={() => rejectNote(note.id)}>Reject</button>
@@ -83,36 +118,7 @@ const CaseNoteApproval = () => {
             ))}
           </tbody>
         </table>
-      )}
-      {selectedNote && (
-        <div className="modal" style={{ display: 'block' }}>
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Note Details</h5>
-                <button type="button" className="btn-close" onClick={() => setSelectedNote(null)}></button>
-              </div>
-              <div className="modal-body">
-                <p><strong>ID:</strong> {selectedNote.id}</p>
-                <p><strong>Author:</strong> {selectedNote.author}</p>
-                <p><strong>Date:</strong> {selectedNote.date}</p>
-                <p><strong>Client:</strong> {selectedNote.client_id}</p>
-                <p><strong>Goals:</strong> {selectedNote.goals}</p>
-                <p><strong>Presentation:</strong> {selectedNote.presentation}</p>
-                <p><strong>Actions:</strong> {selectedNote.actions}</p>
-                <p><strong>Outcome:</strong> {selectedNote.outcome}</p>
-                <p><strong>Follow-up:</strong> {selectedNote.followUp}</p>
-                <p><strong>Follow-up Note:</strong> {selectedNote.followUpNote}</p>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-success" onClick={() => { approveNote(selectedNote.id); setSelectedNote(null); }}>Approve</button>
-                <button type="button" className="btn btn-danger" onClick={() => { rejectNote(selectedNote.id); setSelectedNote(null); }}>Reject</button>
-                <button type="button" className="btn btn-secondary" onClick={() => setSelectedNote(null)}>Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
--- src/WorkerDashboard/WorkerDashboard.jsx
+++ src/WorkerDashboard/WorkerDashboard.jsx
@@ -1,69 +1,96 @@
 import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Body from '../components/body/Body';
-import { authcheck } from "../utilities/authcheck";
 import allcontext from "../context";
+import { Link } from "react-router-dom";
 
+
+const authcheck = async () => {
+  return true;
+};
 
 const WorkerDashboard = () => {
-  const users = useContext(allcontext)[0]
-  const username = users.firstName+' '+users.lastName
+  const navigate = useNavigate();
+  const users = useContext(allcontext)[0];
+  const username = users ? `${users.firstName} ${users.lastName}` : 'Guest';
+
   const workerLists = [
     {
       imsrc: "bi-person-vcard",
       bgcolor: "success",
-      title: "clients",
-      description: "client information, crud",
-      route:"/clients"
+      title: "Clients",
+      description: "Client Information",
+      route: "/clients"
     },
     {
       imsrc: "bi-file-earmark-medical",
       bgcolor: "secondary",
-      title: "roster",
-      description: "roster information, crud",
+      title: "Roster",
+      description: "Check Rosters",
       route: "/roster"
     },
     {
       imsrc: "bi-microsoft-teams",
       bgcolor: "danger",
-      title: "create note",
-      description: "create note for work",
+      title: "Create Note",
+      description: "Create a Progress Note",
       route: "/create-notes"
-    },
+    }
   ];
-  authcheck()
-  const bodyContent =(
+
+  useEffect(() => {
+    authcheck().then((isAuthenticated) => {
+      if (!isAuthenticated) {
+        navigate('/');
+      }
+    });
+  }, [navigate]);
+
+  const handleDivClick = (route) => {
+    navigate(route);
+  };
+
+  const bodyContent = (
     <>
-      <div className="w-75 text-start mx-auto mt-5">
-        Welcome, {username}
+    <div className="container" style={{ maxWidth: '1200px' }}>
+      <div className="text-start mx-auto mt-5">
+        <label className="fs-3">
+          Welcome, 
+          <label className="text-primary fw-light" style={{ paddingLeft: "10px" }}>
+            {username}
+          </label>
+        </label>
         <hr />
-      
-      <div
-        className="w-100 d-flex flex-column justify-content-center align-items-center"
-        style={{ paddingBottom: "100px" }}
-      >
-        {workerLists.map((list, index) => (
           <div
-            className={`w-100 mt-3 border border-${list.bgcolor}`}
-            key={index}
+            className="d-flex flex-column justify-content-center align-items-center"
+            style={{ paddingBottom: "100px" }}
           >
-            <div
-              className={`d-flex justify-content-between p-2 bg-${list.bgcolor} bg-opacity-75`}
-            >
-              <i className={`bi ${list.imsrc} text-white fs-1`}></i>
-              <Link to={list.route}>
-                <label className="fs-2 text-white">{list.title}</label>
-              </Link>
-            </div>
-            <div className="p-2 bg-secondary bg-opacity-25">{list.description}</div>
+            {workerLists.map((list, index) => (
+              <div
+                className={`clickable-div w-100 mt-3 border border-${list.bgcolor}`}
+                style={{ width: '100%' }} 
+                key={index}
+                onClick={() => handleDivClick(list.route)}
+              >
+                <div
+                  className={`d-flex justify-content-between p-2 bg-${list.bgcolor} bg-opacity-75`}
+                >
+                  <i className={`bi ${list.imsrc} text-white fs-1`}></i>
+                  <label className="fs-2 text-white">{list.title}</label>
+                </div>
+                <div className="p-2 bg-secondary bg-opacity-25">{list.description}</div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
       </div>
     </>
   );
 
-  return  <Body body={bodyContent} />;  
+  
+  return <Body body={bodyContent} />;
 };
 
 export default WorkerDashboard;
+
+
